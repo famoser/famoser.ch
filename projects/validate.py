288b625c66fb25078829429b56784b57dbf73ec1
Florian Moser <git@famoser.ch>
2020-08-17 16:43:29 +0200

    working on validation script

---
+++ projects/validate.py
@@ -0,0 +1,101 @@
+import yaml
+import glob
+
+# load projects
+projects = {}
+for filePath in glob.glob("*.yaml"):
+    with open(filePath) as file:
+        project = yaml.load(file)
+        projects[filePath] = project
+
+# save projects
+# do this manually bc I want an easy to understand resulting file
+def addMandatoryLine(lines, dictionary, key):
+    if key not in dictionary:
+        print("Fail: " + key + " not found")
+        return
+    
+    lines.append(key + ": " + dictionary[key])
+
+def addOptionalMultiline(lines, dictionary, key):
+    if key not in dictionary:
+        return
+
+    lines.append(key + ": |")
+    intendation = "  "
+    lines.append(intendation + dictionary[key].replace("\n", intendation + "\n"))
+
+def addOptionalWhitelistedEntry(lines, dictionary, key, whitelist):
+    if key not in dictionary:
+        return
+    
+    if dictionary[key] not in whitelist:
+        print("Fail: " + dictionary[key] + " is not valid for key " + key)
+    
+    lines.append(key + ": " + dictionary[key])
+
+def addOptionalUrlLine(lines, dictionary, key):
+    if key not in dictionary:
+        return
+    
+    if not dictionary[key].startswith("https://"):
+        print("Fail: " + dictionary[key] + " is not a valid https url for key " + key)
+    
+    lines.append(key + ": " + dictionary[key])
+
+def addOptionalMonthLine(lines, dictionary, key):
+    if key not in dictionary:
+        return
+    
+    if len(dictionary[key]) != len("2020-02"):
+        print("Fail: " + dictionary[key] + " is not a valid month for key " + key)
+    
+    lines.append(key + ": " + dictionary[key])
+
+def addOptionalWhitelistedList(lines, dictionary, key, whitelist):
+    if key not in dictionary:
+        return
+
+    for entry in dictionary[entry].values():
+        if entry not in whitelist:
+            print("Fail: " + entry + " is not a valid entry for key " + key)
+    
+    lines.append(key + ": [" + ", ".join(dictionary[key]) + "]")
+
+valid_languages = ["PHP", "C#", "JavaScript", "Python", "Kotlin"]
+valid_frameworks = ["Symfony", "slim", "API Platform", "Sonata Admin", "Vue.js", "Angular", "ASP.NET", "Wordpress"]
+valid_platforms = ["Web", "Windows", "Windows Phone", "Android"]
+valid_employers = ["JKweb", "Zühlke"]
+
+for filePath, project in projects:
+    lines = []
+    addMandatoryLine(lines, project, "name")
+    addMandatoryLine(lines, project, "purpose")
+    addOptionalMultiline(lines, project, "involvement")
+    addOptionalWhitelistedEntry(lines, project, "employer", valid_employers)
+
+    lines.append("")
+    lengthBeforeGroup = len(lines)
+
+    addOptionalUrlLine(lines, project, "public_urL")
+    addOptionalUrlLine(lines, project, "source_url")
+
+    if len(lines) > lengthBeforeGroup:
+        lines.append("")
+    lengthBeforeGroup = len(lines)
+
+    addOptionalMonthLine(lines, project, "kickoff_date")
+    addOptionalMonthLine(lines, project, "publish_date")
+    addOptionalMonthLine(lines, project, "last_activity_date")
+
+    if len(lines) > lengthBeforeGroup:
+        lines.append("")
+    lengthBeforeGroup = len(lines)
+
+    addOptionalWhitelistedList(lines, project, "languages", valid_languages)
+    addOptionalWhitelistedList(lines, project, "frameworks", valid_frameworks)
+    addOptionalWhitelistedEntry(lines, project, "platform", valid_platforms)
+
+    content = "\n".join(lines)
+    with open(filePath, "w") as file:
+        file.write(content)
