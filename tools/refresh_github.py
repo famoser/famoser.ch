bec204180b3cb944e250bf47fa9483ebddf6497b
Florian Moser <git@famoser.ch>
2020-08-18 17:28:10 +0200

    Initial update

--- tools/refresh_github.py
+++ tools/refresh_github.py
@@ -10,19 +10,21 @@ from loader import ProjectLoader
 loader = ProjectLoader()
 projects = loader.load()
 
-github_projects = []
+github_projects = {}
 for project in projects:
-    if "source_url" in project and project["source_url"].startswith("https://github.com")
-        github_projects.append(project)
-
-print(github_projects)
+    if "source_url" in project and project["source_url"].startswith("https://github.com/"):
+        url_length = len("https://github.com/")
+        full_name = project["source_url"][url_length:]
+        github_projects[full_name] = project
 
 from github import Github
 
 github = Github(github_token)
 user = github.get_user()
 for repo in user.get_repos():
-    print(repo.name)
-    
+    if repo.full_name in github_projects:
+        print("updating " + repo.full_name)
+        project = github_projects[repo.full_name]
+        project["purpose"] = repo.description
 
 loader.store(projects)
